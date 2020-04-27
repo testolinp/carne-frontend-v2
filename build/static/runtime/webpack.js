4ea35891e6312b11c253021c6d3c908ecc3a02b7
Patricio Testolin <patricio.testolin@rga.com>
2020-04-27 17:49:31 -0300

    .next files

--- build/static/runtime/webpack.js
+++ build/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9da13f15b0581a203a01";
+/******/ 	var hotCurrentHash = "cb4418c83d2a7dfae709";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
